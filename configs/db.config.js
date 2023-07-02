98c7f436639ceb2eccefea55ff0c1c68ce3bb08f
shiv0212 <shiv1999rajawat@gmail.com>
2023-07-02 17:59:53 +0530

    Day 2 Final code

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_NAME : "idea_app_demo",
+    DB_URL : "mongodb://0.0.0.0/idea_app_demo"
+}
--- server.js
+++ server.js
@@ -1,7 +1,53 @@
 const express = require("express");
 const serverConfig = require("./configs/server.config");
+const mongoose = require("mongoose");
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
 
 const app = express();
+
+//* Logic to connect the mongoDB and create an admin user
+//* need to have mongoDB up and running in your local machine
+
+mongoose.connect(dbConfig.DB_URL)
+const db = mongoose.connection;
+
+db.on("error",()=>{
+    console.log("Error");
+})
+
+db.once("open",()=>{
+    console.log("DB is connected");
+
+    init();
+});
+
+async function init() {
+
+    //* Check if the admin user is already present
+
+    let admin = await userModel.findOne({
+        userId : "admin"
+    });
+
+    if (admin) {
+        console.log("Admin user is already present");
+        return;
+    }
+
+    //* Initialize the mongoDB 
+    //* Need to create admin user
+
+    admin = await userModel.create({
+        name : "Shiva Singh",
+        userId : "admin",
+        email : "shivasingh@gmail.com",
+        userType : "ADMIN",
+        password : bcrypt.hashSync("Welcome1",8)  // 8 is salt
+    });
+    console.log(admin);
+}
+
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server stsrted on the port no ${serverConfig.PORT}`);
